d2208c7229a091a97c92ef6a213dc4d224005a64
Kizito Aorjir <aorjirtk2017@gmail.com>
2024-07-23 11:45:28 +0100

    biggest_odd.py

--- python saturday 20.7.2024/biggest_odd.py
+++ python saturday 20.7.2024/biggest_odd.py
@@ -1,15 +1,9 @@
 
 list_of_integers = []
-
-
 number = str(input("Enter the number "))
-
 for item in number.split(' '):
 	list_of_integers.append(int(item))
-
 	for count in range(1, len(list_of_integers)):
-
 		if count[list_of_integers] % 2 == 1:
-
 			print[list_of_integers]
 		
